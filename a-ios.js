1c851fffd420fe463b671f8e0ca94ae06e41293e
BIN <80822215+BIN-03@users.noreply.github.com>
2026-05-26 20:14:19 +0800

    Update a-ios.js

--- a-ios.js
+++ a-ios.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         海角—解锁金币/钻石
-// @version      1.0.32
+// @version      1.0.33
 // @description  ⚡支持观看及下载视频，已移除付费金币/钻石，直接使用。⚡
 // @author      作者703860120
 // @icon        https://www.haijiao.com/images/common/project/loading.gif
